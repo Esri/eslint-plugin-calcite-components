cc8e45bff98ddde28c6196112a00c01df4392eff
JC Franco <jfranco@esri.com>
2022-07-15 14:15:13 -0700

    feat(rule): add ban-events rule

---
+++ tests/lib/rules/ban-events/ban-events.good.tsx
@@ -0,0 +1,19 @@
+@Component({ tag: 'sample-tag' })
+export class SampleTag {
+  @Listen("keydown")
+  onAllowedEvent() {}
+
+  connectedCallback(): void {
+    document.addEventListener("click", this.handleAllowedEvent);
+  }
+
+  disconnectedCallback(): void {
+    document.removeEventListener("click", this.handleAllowedEvent);
+  }
+
+  handleAllowedEvent(): void {}
+
+  render() {
+    return (<div>test</div>);
+  }
+}
